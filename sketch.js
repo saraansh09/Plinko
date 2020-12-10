43dc063dcdb7d57ea154640e7fec8cd1a8d4d131
saraansh09 <73740049+saraansh09@users.noreply.github.com>
2020-12-10 16:14:10 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,90 @@
+const Engine = Matter.Engine,
+      World = Matter.World,
+      Events = Matter.Events,
+      Bodies = Matter.Bodies
+
+var engine,world; 
+var particles = [];
+var plinkos=[];
+var rows=[];
+var ground1,ground2,ground3,ground4;
+var rows;
+
+
+var rowsHeight = 150;
+
+function setup() {
+
+  
+  createCanvas(600,500);
+  
+  engine = Engine.create();
+  world = engine.world;
+
+  ground1 = new Ground(width / 2, 500, width, 10);
+  ground2 = new Ground(600, height / 2, 10, height);
+  ground3 = new Ground(width / 2, 0, width, 10);
+  ground4 = new Ground(0, height / 2, 10, height)
+
+  for(var j = 40; j<= width; j = j+50){
+    plinkos.push(new Plinko(j , 55))
+  }
+  for(var j = 15; j<= width-10; j = j+50){
+    plinkos.push(new Plinko(j ,85))
+  }
+  for(var j = 40; j<= width-10; j = j+50){
+    plinkos.push(new Plinko(j ,120))
+  }
+  for(var j = 15; j<= width-10; j = j+50){
+    plinkos.push(new Plinko(j ,150))
+  }
+  for(var j = 40; j<= width-10; j = j+50){
+    plinkos.push(new Plinko(j ,185))
+  }
+  for(var j = 15; j<= width-10; j = j+50){
+    plinkos.push(new Plinko(j ,225))
+  }
+  for(var j = 40; j<= width-10; j = j+50){
+    plinkos.push(new Plinko(j ,265))
+  }
+  for(var j = 15; j<= width-10; j = j+50){
+    plinkos.push(new Plinko(j ,305))
+  }
+  for(var k = 0; k <=width; k = k+80){
+    rows.push(new Rows(k, height-rowsHeight/2, 10, rowsHeight));
+  }
+}
+
+function draw() {
+  background(0);
+  if(frameCount % 30 === 0){
+    particle = new Particle(random(130, 400), 0, 7, random(0, 360));
+    particles.push(particle);
+  }
+
+  
+  Engine.update(engine, 25);
+  for(var i = 0; i < particles.length; i++){
+    particles[i].display();
+  }
+
+ 
+  for(var k = 0; k<rows.length;k++){
+    rows[k].display();
+ }
+
+
+  for(var j = 0; j<plinkos.length;j++){
+    plinkos[j].display();
+ }
+
+
+//rows.display();
+ ground1.display();
+ ground2.display();
+ ground3.display();
+ ground4.display();
+
+
+  drawSprites();
+}
